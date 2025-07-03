f6ce2f36a0a51de60547a04e20555c7010f45f17
Hiruyan-J <hiruyan0428@gmail.com>
2025-07-04 08:29:03 +0900

    update: アプリケーションの設定

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,11 @@ module Myapp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators.system_tests = nil
+    config.generators do |g|
+      g.skip_routes true # ルーティング追記をスキップ
+      g.helper false # ヘルパーメソッドの追加無し
+      g.test_framework nil # テストフレームワークを使わない
+    end
   end
 end
