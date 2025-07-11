927584b437312d6176ee1a3bc26aae0506afb7bb
Hiruyan-J <hiruyan0428@gmail.com>
2025-07-11 23:08:30 +0900

    update: 入力エラーメッセージをアラート表示に設定

--- app/controllers/sake_logs_controller.rb
+++ app/controllers/sake_logs_controller.rb
@@ -1,8 +1,8 @@
 class SakeLogsController < ApplicationController
   def index
-    flash.now[:info] = 'info_test'
-    flash.now[:success] = 'success_test'
-    flash.now[:warning] = 'wornig_test'
-    flash.now[:error] = 'error_test'
+    flash.now[:info] = "info_test"
+    flash.now[:success] = "success_test"
+    flash.now[:warning] = "wornig_test"
+    flash.now[:error] = "error_test"
   end
 end
