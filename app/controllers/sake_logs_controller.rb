d296c005501dee38071de1588e2db0b42b22bd16
Hiruyan-J <hiruyan0428@gmail.com>
2025-07-12 13:44:28 +0900

    update: Devise標準のフラシュタイプをDaisyUIのCSSクラスで表示させる

--- app/controllers/sake_logs_controller.rb
+++ app/controllers/sake_logs_controller.rb
@@ -1,8 +1,4 @@
 class SakeLogsController < ApplicationController
   def index
-    flash.now[:info] = "info_test"
-    flash.now[:success] = "success_test"
-    flash.now[:warning] = "wornig_test"
-    flash.now[:error] = "error_test"
   end
 end
