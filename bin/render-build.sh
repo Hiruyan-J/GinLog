5f1e2df1595990efc823e09491a8205d4ce5f40c
Hiruyan-J <hiruyan0428@gmail.com>
2025-07-13 21:56:27 +0900

    update: コマンドの修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,4 +5,6 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 # bundle exec rails db:migrate
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
+RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop
+rails db:create RAILS_ENV=production
+rails db:migrate RAILS_ENV=production
