6be2ebfdc512139fb84049c9706c78cc49f87273
Hiruyan-J <hiruyan0428@gmail.com>
2025-07-16 14:24:37 +0900

    update: 仮新規投稿画面

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  resources :sake_logs, only: %i[index, new, create]
+  resources :sake_logs, only: %i[index new create]
 
   # Defines the root path route ("/")
   root "sake_logs#index"
