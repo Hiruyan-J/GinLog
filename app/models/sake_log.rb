07800f9a2fdd3f2d3d6c38058390bdf27e740ec8
Hiruyan-J <hiruyan0428@gmail.com>
2025-07-19 22:20:00 +0900

    update: 定数の定義場所変更

--- app/models/sake_log.rb
+++ app/models/sake_log.rb
@@ -23,9 +23,7 @@
 #  fk_rails_...  (user_id => users.id)
 #
 class SakeLog < ApplicationRecord
-  RATING_MIN = 0
-  RATING_MAX = 5
-  validates :rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: RATING_MIN, less_than_or_equal_to: RATING_MAX }
+  validates :rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: RATING_MIN = 0, less_than_or_equal_to: RATING_MAX = 5 }
   validates :taste_strength, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
   validates :aroma_strength, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
   validates :review, length: { maximum: 65_535 }
