5f1f7bf4be8a616bc1fe97c7211fdef6c5dc9987
Hiruyan-J <hiruyan0428@gmail.com>
2026-02-11 18:44:35 +0900

    update: Lintチェック対応

--- config/initializers/field_error_proc.rb
+++ config/initializers/field_error_proc.rb
@@ -2,8 +2,8 @@ ActionView::Base.field_error_proc = Proc.new do |html_tag, _instance|
   doc = Nokogiri::HTML::DocumentFragment.parse(html_tag)
   doc.children.each do |node|
     if node.element?
-      existing = node['class'].to_s
-      node['class'] = "#{existing} field_with_errors".strip
+      existing = node["class"].to_s
+      node["class"] = "#{existing} field_with_errors".strip
     end
   end
   doc.to_html.html_safe
