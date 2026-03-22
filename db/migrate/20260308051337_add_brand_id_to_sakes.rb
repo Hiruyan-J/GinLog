6f8010f108aa1034dce85e1506b0b9d69bd569bd
Hiruyan-J <hiruyan0428@gmail.com>
2026-03-22 20:17:43 +0900

    chore: Rubocopスタイル修正

--- db/migrate/20260308051337_add_brand_id_to_sakes.rb
+++ db/migrate/20260308051337_add_brand_id_to_sakes.rb
@@ -1,6 +1,6 @@
 class AddBrandIdToSakes < ActiveRecord::Migration[7.2]
   def change
     add_reference :sakes, :brand, null: true, foreign_key: true
-    add_index :sakes, [:brand_id, :product_name], unique: true
+    add_index :sakes, [ :brand_id, :product_name ], unique: true
   end
 end
