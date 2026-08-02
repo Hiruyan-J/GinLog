ea98af374bbc500c5bbe9963639b66b88663c6ab
Hiruyan-J <hiruyan0428@gmail.com>
2026-08-02 13:51:05 +0900

    docs: Active Storage の保存先コメントを Cloudinary に修正
    保存先を :cloudinary に変更した際、Rails 生成時の
    「ローカルファイルシステムに保存する」というコメントが残っていた。

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   # Change to :null_store to avoid any caching.
   config.cache_store = :memory_store
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
+  # Store uploaded files on Cloudinary (see config/storage.yml for options).
   config.active_storage.service = :cloudinary
 
   # Don't care if the mailer can't send.
